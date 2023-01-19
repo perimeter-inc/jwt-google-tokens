a35ac8aee779a46a5e588d55fb2104738fc7d828
Thales César <thalesgiriboni@gmail.com>
2023-01-19 11:17:52 -0300

    Replace compile-time config reading for run-time

--- lib/plugs/verification.ex
+++ lib/plugs/verification.ex
@@ -1,12 +1,12 @@
 defmodule Jwt.Plugs.Verification do
-  @expired_token_error {:error, "Expired token."}
   @timeutils Application.get_env(:jwt, :timeutils, Jwt.TimeUtils)
   @five_minutes 5 * 60
 
   def default_options(), do: %{:ignore_token_expiration => false, :time_window => @five_minutes}
 
   def verify_token(token, opts) do
-    verify_signature(token)
+    token
+    |> verify_signature()
     |> verify_expiration(opts)
   end
 
@@ -20,10 +20,10 @@ defmodule Jwt.Plugs.Verification do
 
     cond do
       ignore_token_expiration -> {:ok, claims}
-      now > expiration_date -> @expired_token_error
+      now > expiration_date -> {:error, "Expired token."}
       now <= expiration_date -> {:ok, claims}
     end
   end
 
-  defp verify_expiration({:error, _}, _opts), do: @expired_token_error
+  defp verify_expiration({:error, _} = error, _opts), do: error
 end
